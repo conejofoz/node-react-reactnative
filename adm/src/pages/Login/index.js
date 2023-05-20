a6e0890b469b4503fdfbf3d2f102677273d61520
conejofoz <conejofoz@gmail.com>
2023-05-20 10:57:56 -0300

    implementado o layout da página de login

--- adm/src/pages/Login/index.js
+++ adm/src/pages/Login/index.js
@@ -1,13 +1,32 @@
 import React from 'react'
-import { Alert } from 'reactstrap'
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
+import '../../styles/login/index.css'
 
 export default function Login(){
     return (
         <div>
-            <h1>Login</h1>
-            <Alert color='primary'>
-                Bem vindo
-            </Alert>
+            <div className='login card shadow'>
+                <Form className='form-signin text-center'>
+                    <img className='mb-4' src="images/logo_celke.png" alt='' width="72" height="72" />
+                    <h1 className="h3 mb-3 font-weight-normal">Área Restrita</h1>
+
+                    <FormGroup>
+                        <Label for='email'>Usuário</Label>
+                        <Input type='email' name='email' id='email' placeholder='Email do usuário'/>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for='senha'>Senha</Label>
+                        <Input type='senha' name='senha' id='senha' placeholder='Senha do usuário'/>
+                    </FormGroup>
+
+                    <Button color='primary w-100' >Acessar</Button>
+                    <p className='text-center mt-2'>Esqueceu a senha?</p>
+
+                </Form>
+
+            </div>
+            
         </div>
     )
 }
